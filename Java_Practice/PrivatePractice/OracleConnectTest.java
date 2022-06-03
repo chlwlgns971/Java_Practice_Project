650368a1b487aafeb51fc717f17f5fd7b597b563
chlwlgns971 <chlwlgns971@gmail.com>
2022-06-03 20:53:13 +0900

    2022.06.03 WindowBuilderTest 변경점-> J캘린더 추가 OracleConnectTest 변경점-> 오라클DB연동 성공 및 기존 DB에서 컬럼 Search 테스트

--- Java_Practice/PrivatePractice/OracleConnectTest.java
+++ Java_Practice/PrivatePractice/OracleConnectTest.java
@@ -1,6 +1,8 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.ResultSet;
 
 public class OracleConnectTest {
 	public static void main(String[] args) {
@@ -9,15 +11,36 @@ public class OracleConnectTest {
 		String url = "jdbc:oracle:thin:@localhost:1521:xe";
 		String user = "ddit";
 		String password = "java";
+		Connection conn=null;
+		Statement stat = null;
 		try {
 			Class.forName(driver);
 			System.out.println("jdbc driver 로딩 성공");
-			DriverManager.getConnection(url, user, password);
+			conn=DriverManager.getConnection(url, user, password);
 			System.out.println("오라클 연결 성공");
+			stat=conn.createStatement();
+			
+			//쿼리 작성
+			ResultSet rs = stat.executeQuery("SELECT* FROM MEMBER");
+			while (rs.next()) {
+				//속성명을 입력해야지 출력가능 -> 검색할 select문을 선택문으로 만들고 선택한 속성은 문자화해서 출력하는걸로 해야함.
+				System.out.println(rs.getString("MEM_ID"));
+				System.out.println(rs.getString("MEM_NAME"));
+				System.out.println(rs.getString("MEM_ADD1"));
+				System.out.println(rs.getString("MEM_JOB"));
+				
+                //인덱스로도 접근가능. 가독성엔 취약.
+//				System.out.println(rs.getString(1));
+//				System.out.println(rs.getString(2));
+//				System.out.println(rs.getString(3));
+//				System.out.println(rs.getString(4));
+			}
+			rs.close();  //커서 닫기
+			conn.close();
 		} catch (ClassNotFoundException e) {
 			System.out.println("jdbc driver 로딩 실패");
 		} catch (SQLException e) {
 			System.out.println("오라클 연결 실패");
-		}
+		} 
 	}
 }
