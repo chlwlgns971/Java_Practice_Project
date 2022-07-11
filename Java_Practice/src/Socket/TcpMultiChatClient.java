b1b13e71dc9c67f92e153c066a96be2a5eeba8c5
chlwlgns971 <chlwlgns971@gmail.com>
2022-07-11 11:28:11 +0900

    2022.07.11 소켓통신을 활용한 멀티채팅 구현

---
+++ Java_Practice/src/Socket/TcpMultiChatClient.java
@@ -0,0 +1,123 @@
+package Socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.Socket;
+import java.util.Scanner;
+
+import Chapter10.TryCatch01;
+
+public class TcpMultiChatClient {
+
+	public static void main(String[] args) {
+		new TcpMultiChatClient().clientStart();
+	
+	} 
+    private void clientStart() {
+    	Socket socket = null;
+      
+     	try {
+     		socket = new Socket("localhost", 7777);
+     		System.out.println("서버에 연결되었습니다");
+     		System.out.println();
+     		
+     		//메세지 전송용 쓰레드 생성
+     		ClientSender sender = new ClientSender(socket);
+     		//메세지 수신용 쓰레드 생성
+            ClientReceiver receiver = new ClientReceiver(socket);
+            
+            sender.start();
+            receiver.start();
+
+     	}catch(Exception e) {
+     		
+     	}
+
+
+	}
+    // 메시지 전송용 쓰레드
+    class ClientSender extends Thread{
+       private Socket socket;
+       private DataInputStream din;
+       private DataOutputStream dout;
+       private String name;
+       private Scanner sc;
+       
+       //생성자
+       public ClientSender(Socket socket) {
+    	   this.socket=socket;
+    	   sc=new Scanner(System.in);
+    	   try {
+    		   //수신용
+    		   din=new DataInputStream(socket.getInputStream());
+    		   //송신용
+    		   dout=new DataOutputStream(socket.getOutputStream());
+    		   
+    		   if(din!=null) {
+    			   do {
+    				   //클라이언트용 프로그램은 처음 실행하면 서버에 접속하고 접속이 성공하면 대화명을 입력받아 서버로 전송
+    				   //서버로부터 대화명의 중복여부를 응답으로 받는다.
+    				   System.out.println("대화명: ");
+    				   String name=sc.nextLine();
+    				   dout.writeUTF(name);
+    				   
+    				   //대화명의 중복여부를 응답으로 받는다.
+    				   String feedback=din.readUTF();
+    				   if("대화명중복".equals(feedback)) {
+    					   System.out.println(name+"은 대화명이 중복됩니다.");
+    					   System.out.println("다른 대화명을 입력하세요.");
+    				   }
+    				   else {
+    					   this.name=name;
+    					   System.out.println(name+" 대화명으로 대화방에 입장했습니다...");
+    					   break;
+    				   }
+    			   }while(true);
+    		   }
+    		  
+    	   } catch (Exception e) {
+			// TODO: handle exception
+    	   }
+    	   
+       }
+       @Override
+       public void run() {
+          try {
+             while(dout !=null) {
+                //키보드로 입력한 메시지를 서버로 전송한다.
+                dout.writeUTF("[" + name + "]" + sc.nextLine());
+             }   
+          } catch (Exception e) {
+             // TODO: handle exception
+          }
+       }
+    } //전송용 쓰레드 끝...
+    
+    //메세지 수신용 쓰레드
+    class ClientReceiver extends Thread {
+        private Socket socket;
+        private DataInputStream din;
+
+        // 생성자
+        public ClientReceiver(Socket socket) {
+           this.socket = socket;
+
+           try {
+              din = new DataInputStream(socket.getInputStream());
+           } catch (Exception e) {
+              // TODO: handle exception
+           }
+        } // 생성자 끝
+
+        @Override
+        public void run() {
+           try {
+              while (din != null) {
+                 System.out.println(din.readUTF());
+              }
+           } catch (Exception e) {
+              // TODO: handle exception
+           }
+        }
+     }
+}
---
+++ Java_Practice/src/Socket/TcpMultiChatServer.java
@@ -0,0 +1,122 @@
+package Socket;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TcpMultiChatServer {
+	//접속한 클라리언트 정보를 저장할 Map객체 선언
+	//->키 값: 접속한 사람 대화명, value값: 접속한 클라이언트의 Socket객체
+	private Map<String, Socket> clientMap;
+	
+	//생성자
+	public TcpMultiChatServer() {
+		//clientMap을 동기화 처리가 되도록 한다.
+		clientMap=Collections.synchronizedMap(new HashMap<String, Socket>());
+	}
+	public static void main(String[] args) {
+		new TcpMultiChatServer().serverStart();
+
+	}
+	public void serverStart() {
+		ServerSocket server=null;
+		Socket socket=null;
+		try {
+			server=new ServerSocket(7777);
+			System.out.println("서버가 시작되었습니다.");
+			
+			while(true) {
+				socket=server.accept();
+				System.out.println("["+socket.getInetAddress()+":"+socket.getPort()+"]에서 접속했습니다.");
+				 //서버용 쓰레드 객체를 생성한 후 실행시킨다.
+	            ServerReceiver serverThread = new ServerReceiver(socket);
+	            serverThread.start();
+
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
+	//clientMap에 저장된 전체 사용자에게 메시지를 전송하는 메서드
+	private void sendToAll(String msg) {
+		//clientMap의 데이터 개수만큼 반복
+		for(String key: clientMap.keySet()) {
+			try {
+				//key값에 대응하는 Socket객체의 출력용 스트림
+				//이용한 출력용 스트림 객체 생성
+				DataOutputStream dout=new DataOutputStream(clientMap.get(key).getOutputStream());
+				dout.writeUTF(msg);
+			} catch (Exception e) {
+				e.printStackTrace();
+				
+			}
+		}
+	}
+	//Inner Class로 서버에서 클라이언트로 메시지를 전송하는 Thread 작성하기-> Outter Class의 멤버변수를 사용하기 위해서
+	class ServerReceiver extends Thread{
+		private Socket socket;
+		private DataInputStream din;
+		private DataOutputStream dout;
+		
+		//생성자
+		public ServerReceiver(Socket socket) {
+			this.socket=socket;
+			try {
+				//수신용 스트림 객체 생성
+				din=new DataInputStream(socket.getInputStream());
+				//송신용 스트림 객체 생성
+				dout=new DataOutputStream(socket.getOutputStream());
+			}catch(Exception e) {
+				
+			}
+		}
+		@Override
+		public void run() {
+			String name=""; //접속한 클라이언트의 대화면이 저장될 변수
+			try {
+				//클라이언트가 연결이 성공되면 첫 번째 데이터로 '대화명'을 입력받아 보낸다.
+				//서버에서는 이 '대화명'의 중복여부를 판단하여 응답한다.
+				
+				//클라이언트가 보낸 '대화명'이 중복되지 않을 때까지 반복하는 반복문
+				while(true) {
+					name=din.readUTF();//클라이언트가 보내온 '대화명'받기
+					
+					//'대화명'의 중복 여부 확인
+					if(clientMap.containsKey(name)) {
+						dout.writeUTF("대화명중복"); //대화명중복 메시지 응답으로 보내기
+					}
+					else {
+						dout.writeUTF("OK"); //중복되지 않을시 OK메세지를 응답으로 보내기
+						break;
+					}
+				}
+				//접속한 사람의 대화명을 이용하여 다른 모든 접속자들에게 대화방 참여 메시지를 전송한다.
+				sendToAll("["+name+"]님이 대화방에 입장했습니다.");
+				
+				//접속한 사람의 대화명과 SOcket객체를 clientMap에 추가한다.
+				clientMap.put(name, socket);
+				System.out.println("현재 접속자 수: "+clientMap.size()+"명");
+				
+				//한 클라이언트가 보낸 메세지를 받아서 전체 클라이언트들에게 보낸다.
+				while(din!=null) {
+					sendToAll(din.readUTF());
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+			}finally {
+				//finally영역이 실행된다는 것은 클라이언트가 접속을 종료햇다는 의미이다.
+				sendToAll("["+name+"]님이 접속을 종료했습니다.");
+				
+				//clientMap에서 해당 대화명을 삭제한다.
+				clientMap.remove(name);
+				System.out.println("["+socket.getInetAddress()+":"+socket.getPort()+"]에서 접속을 종료했습니다.");
+				System.out.println("현재 접속자 수: "+clientMap.size()+"명");
+			}
+		}
+	}
+
+}
